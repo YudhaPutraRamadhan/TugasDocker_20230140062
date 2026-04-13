9552694f09f992cc84d0de97a9cb591f9427b22e
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2026-04-13 10:17:18 +0700

    Membuat variabel class User untuk menampung data-data mahasiswa

---
+++ src/main/java/com/example/pertemuan6/model/User.java
@@ -0,0 +1,14 @@
+package com.example.pertemuan6.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String Nama;
+    private String NIM;
+    private String JenisKelamin;
+}
